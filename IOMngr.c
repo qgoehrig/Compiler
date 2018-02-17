53a97f9f0d243d04c5c70dbd33dc8926b6ed20cf
Quentin Goehrig <goehrig.quentin@uwlax.edu>
2018-02-17 13:00:31 -0800

    gulp buffer

--- IOMngr.c
+++ IOMngr.c
@@ -30,91 +30,90 @@ int messageCnt;
 bool OpenSource(const char * aFileName, bool mode) {
     sourceFile = fopen(aFileName, "r");
     echoMode = mode;
-    printf("Opened Source with with echoMode: ");
-    printf(echoMode ? "on\n" : "off\n");
+    //printf("Opened Source with with echoMode: ");
+    //printf(echoMode ? "on\n" : "off\n");
     messageCnt, nextPos = 0;
-    //bufLen = MAXLINE + (26 * 3); //for the extra highlighted
     bufLen = 0;
-    curLine = 1;
+    curLine = 0;
     return sourceFile != NULL;
 }
 
 void CloseSource() {
-    fclose( sourceFile );
-    // perform other final actions
+    fclose( sourceFile ); // perform other final actions
+}
+
+void printMessages() {
+    for(int i = 0; i < messageCnt; i++) {
+        printf("       -%c %s\n", (char) i + 65, messages[i].message);
+    }
+}
+bool init = false;
+bool finished = false;
+bool highlight = false;
+
+bool fillBuffer() {
+    //printf("filling\n");
+    char * valid = fgets( buffer, MAXLINE, sourceFile );
+    bufLen = strlen(buffer);
+    curLine++;
+    nextPos = 0;
+    //printf("valid?=%d", valid != NULL);
+    return valid != NULL;
 }
 
 char GetSourceChar() {
-    char cChar = fgetc( sourceFile );
-    buffer[bufLen] = cChar;
-    nextPos++;
-    bufLen++;
-    struct message nextMessage;
-    if(cChar == '\n') {
-        //check for echomode
-        char printChar = 65; // A
-        int bufferIndex = 0;
-        nextPos = 1;
-        int msgIndex = 0;
-        int nextMsgStart, nextMsgEnd;
-        nextMessage = messages[msgIndex];
-        nextMsgStart = nextMessage.startColumn;
-        nextMsgEnd = nextMessage.endColumn;
-        bool onToken = false;
-        //printf("%d: ", curLine);
-
-        while( bufferIndex < bufLen ) {
-            char cChar = buffer[nextPos];
-            // Encounter start of token message
-            if( !onToken && (msgIndex < messageCnt) && (nextPos == nextMsgStart) ) {
-                // Print letter and start underlining
-                printf("\033[7m %c \033[0m\033[4m", printChar);
-                onToken = true;
-                printChar++;
-            }
-            // Encounter end of token message
-            else if( onToken && (bufferIndex == nextMsgEnd) ) {
-                printf("%c", cChar);
-                printf("\033[0m"); // Print current char and stop underlining
-                onToken = false;
-                msgIndex++;
-                nextMessage = messages[msgIndex];
-                nextMsgStart = nextMessage.startColumn;
-                nextMsgEnd = nextMessage.endColumn;
-            }
-            // Encounter reg character
-            else {
-                printf("%c", buffer[bufferIndex]);
-            }
-
-            bufferIndex++;
-            nextPos++;
-        }
-        // Finish
-        memset(buffer, 0, MAXLINE); // Reset buffer
-        bufLen, messageCnt, msgIndex, bufferIndex = 0; // Reset vars
-        msgIndex = 0;
-        curLine++;
-        nextPos = 1;
-        bufferIndex = 0;
+    if( !init ) {
+        fillBuffer();
+        init = true;
     }
-    return cChar;
+    if( nextPos == bufLen ) {
+        printf("    %d: ", curLine);
+        int msgIndex = 0;
+        struct message msg = messages[msgIndex];
+        for(int i = 0; i < bufLen; i++) {
+            if(msgIndex < messageCnt && msg.startColumn == i + 1) {
+                printf("\033[7m %c \033[0m", (char) msgIndex + 65);
+                msgIndex++;
+                msg = messages[msgIndex];
+            }
+            printf("%c", buffer[i]);
+        }
+        printMessages();
+        messageCnt = 0;
+        if( !fillBuffer() ) {
+            //printf("DONE YOU PIECE OF SHIT\n");
+            finished = true;
+        }
+    }
+    char c = finished ? EOF : buffer[nextPos];
+    //printf("%c", c);
+    // if( c == '\n' ) {
+    //     // printf("    %d: msgs: %d", curLine, messageCnt);
+    //     // int msgIndex = 0;
+    //     // struct message msg = messages[msgIndex];
+    //     for(int i = 0; i < bufLen; i++) {
+    //         // if(msgIndex < messageCnt && msg.startColumn == i + 1) {
+    //         //     printf("\033[7m %c \033[0m", (char) msgIndex + 65);
+    //         //     msgIndex++;
+    //         //     msg = messages[msgIndex];
+    //         // }
+    //         printf("%c", buffer[i]);
+    //     }
+    //     //printf("    %d: %s", curLine, buffer);
+    //     messageCnt = 0;
+    // }
+    nextPos++;
+    return c;
 }
 
 void PostMessage(int aColumn, int aLength, const char * aMessage) {
-    //printf("col: %d, len: %d, msg: %s\n", aColumn, aLength, aMessage);
-    //printf("%s", aMessage);
-    char * sequence = malloc( aLength + 4 ); //
-    char start = buffer[aColumn];
+    //check prev msg
+    //printf("COL: %d, LEN: %d, MSG: %s\n", aColumn, aLength, aMessage);
     struct message cMessage = {
         aColumn, (aColumn + aLength), strdup(aMessage)
     };
-    /*cMessage->message = (aMessage) ? strdup(aMessage) : NULL;
-    cMessage->startColumn = aColumn;
-    cMessage->endColumn = aColumn + aLength; //don't think need the -1*/
     messages[messageCnt] = cMessage;
     messageCnt++;
-
 }
 
 int GetCurrentLine() {
