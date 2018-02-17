5522d94bc607d75958b046af850c7b9c8994a2d1
Quentin Goehrig <goehrig.quentin@uwlax.edu>
2018-02-17 15:38:46 -0800

    working state

--- IOMngr.c
+++ IOMngr.c
@@ -1,7 +1,7 @@
 /* Author:      Quentin Goehrig
    Created:     02/12.18
-   Resources:   https://linux.die.net/man/3/memmove - memmove
-                http://man7.org/linux/man-pages/man3/printf.3.html - print format
+   Resources:   https://linux.die.net/man/3/memmove - memmove (not used)
+                http://man7.org/linux/man-pages/man3/printf.3.html - printformat
 */
 
 #include <stdio.h>
@@ -16,8 +16,7 @@ FILE *sourceFile;
 char buffer[MAXLINE];
 int bufLen, nextPos;
 int curLine;
-bool echoMode; //If true,
-bool needsDisplay;
+bool echoMode;
 
 struct message {
   int startColumn;
@@ -28,14 +27,15 @@ struct message {
 struct message messages[MAX_MESSAGES];
 int messageCnt;
 
+bool init = false;
+bool finished = false;
+bool highlighting = false;
+int highlightEndCol = -1;
+
 bool OpenSource(const char * aFileName, bool mode) {
     sourceFile = fopen(aFileName, "r");
     echoMode = mode;
-    //printf("Opened Source with with echoMode: ");
-    //printf(echoMode ? "on\n" : "off\n");
-    messageCnt, nextPos = 0;
-    bufLen = 0;
-    curLine = 0;
+    messageCnt, nextPos, bufLen, curLine = 0;
     return sourceFile != NULL;
 }
 
@@ -44,68 +44,60 @@ void printMessages() {
         printf("       -%c %s\n", (char) i + 65, messages[i].message);
     }
 }
-bool init = false;
-bool finished = false;
-bool highlighting = false;
-int highlightEndCol = -1;
+
+// Prints source line, token identifiers and messages according to echo rules
+void printLineAndMessages() {
+    if( !echoMode || messageCnt > 0) {
+        printf("%*d: ", 5, curLine);
+    }
+    int msgIndex = 0;
+    struct message msg = messages[msgIndex];
+    for(int i = 0; i < bufLen; i++) {
+        if( highlighting && highlightEndCol == i + 1) {
+            printf("\033[0m");
+            highlighting = false;
+        }
+        else if(msgIndex < messageCnt && msg.startColumn == i + 1) {
+            printf("\033[7m %c \033[0m\033[4m", (char) msgIndex + 65);
+            highlightEndCol = msg.endColumn;
+            highlighting = true;
+            msgIndex++;
+            msg = messages[msgIndex];
+        }
+        if( !echoMode || messageCnt > 0 ) {
+            printf("%c", buffer[i]);
+        }
+    }
+    printMessages();
+}
 
 void CloseSource() {
-    if( !finished ) { //flush buffer and print messages
-        printf("%s", buffer);
-        printf("%s", messages[0].message);
+    if( !finished ) {
+        printLineAndMessages();
     }
+    //clear memory?
     fclose( sourceFile );
 }
 
+// Returns true when line is not at EOF, otherwise false
 bool fillBuffer() {
-    //printf("filling\n");
     char * valid = fgets( buffer, MAXLINE, sourceFile );
     bufLen = strlen(buffer);
     curLine++;
     nextPos = 0;
-    //printf("valid?=%d", valid != NULL);
     return valid != NULL;
 }
 
 char GetSourceChar() {
-    if( curLine == 3 ) {
-        char * c = strdup(buffer);
-        int a = 3;
-        int np = nextPos;
-        int bl = bufLen;
-    }
     if( !init ) {
         fillBuffer();
         init = true;
     }
     if( nextPos == bufLen ) {
-        if( !echoMode || messageCnt > 0) {
-            printf("%*d: ", 5, curLine);
-        }
-        int msgIndex = 0;
-        struct message msg = messages[msgIndex];
-        for(int i = 0; i < bufLen; i++) {
-            if( highlighting && highlightEndCol == i + 1) {
-                printf("\033[0m");
-                highlighting = false;
-            }
-            else if(msgIndex < messageCnt && msg.startColumn == i + 1) {
-                printf("\033[7m %c \033[0m\033[4m", (char) msgIndex + 65);
-                highlightEndCol = msg.endColumn;
-                highlighting = true;
-                msgIndex++;
-                msg = messages[msgIndex];
-            }
-            if( !echoMode || messageCnt > 0 ) {
-                printf("%c", buffer[i]);
-            }
-        }
-        printMessages();
+        printLineAndMessages();
         messageCnt = 0;
         if( !fillBuffer() ) {
-            //printf("cunt\n");
             finished = true;
-            //flush buffer
         }
     }
     char c = finished ? EOF : buffer[nextPos];
@@ -113,14 +105,19 @@ char GetSourceChar() {
     return c;
 }
 
+// Checks for overlaping messages and 26 message limit before posting message
 void PostMessage(int aColumn, int aLength, const char * aMessage) {
-    //check prev msg
-    //printf("COL: %d, LEN: %d, MSG: %s\n", aColumn, aLength, aMessage);
-    struct message cMessage = {
-        aColumn, (aColumn + aLength), strdup(aMessage)
-    };
-    messages[messageCnt] = cMessage;
-    messageCnt++;
+    int prevMsgEndCol = 0;
+    if( messageCnt > 0) {
+        prevMsgEndCol = messages[messageCnt - 1].endColumn;
+    }
+    if( messageCnt == 0 || ((messageCnt < 26) && (prevMsgEndCol <= aColumn)) ){
+        struct message cMessage = {
+            aColumn, (aColumn + aLength), strdup(aMessage)
+        };
+        messages[messageCnt] = cMessage;
+        messageCnt++;
+    }
 }
 
 int GetCurrentLine() {
