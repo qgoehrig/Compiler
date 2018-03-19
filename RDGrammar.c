45e113d8aad279e3a75dcc184804915dd49b8d8b
Quentin Goehrig <Goehrig.Quentin@uwlax.edu>
2018-03-18 22:28:43 -0500

    passing test cases i think

--- RDGrammar.c
+++ RDGrammar.c
@@ -8,7 +8,6 @@ bool Prog()
 // <Prog>     :==  Ident { <StmtSeq> }
 {
   ENTER;
-
   if (!Match(IDENT_TOK)) return LEAVE_FAIL;
   if (!Match(LBRACE_TOK)) return LEAVE_FAIL;
   if (!StmtSeq()) return LEAVE_FAIL;
@@ -22,11 +21,13 @@ bool StmtSeq()
 // <StmtSeq>  :==
 {
   ENTER;
-
   switch (CurrentToken()) {
   case INT_TOK:
   case CHR_TOK:
   case IDENT_TOK:
+    if(!Stmt()) return LEAVE_FAIL;
+    if(!Match(SEMI_TOK)) return LEAVE_FAIL;
+    if(!StmtSeq()) return LEAVE_FAIL;
     break;
   case RBRACE_TOK:
     break;
@@ -35,7 +36,6 @@ bool StmtSeq()
     return LEAVE_FAIL;
     break;
   }
-
   return LEAVE_SUCC;
 }
 
@@ -44,57 +44,143 @@ bool Stmt()
 // <Stmt>    :== <Assign>
 {
   ENTER;
-
-  return LEAVE_SUCC;
+  if(Decl()) return LEAVE_SUCC;
+  if(Assign()) return LEAVE_SUCC;
+  return LEAVE_FAIL;
 }
 
 bool Decl()
 // <Decl>    :== <Type> <IDLst>              INT CHR
 {
+  ENTER;
+  if(!Type()) return LEAVE_FAIL;
+  if(!IDLst()) return LEAVE_FAIL;
+  return LEAVE_SUCC;
 }
 
 bool Type()
 // <Type>    :== INT
 // <Type>    :== CHR
 {
+    ENTER;
+    switch(CurrentToken()) {
+      case INT_TOK:
+        if(!Match(INT_TOK)) return LEAVE_FAIL;
+        break;
+      case CHR_TOK:
+        if(!Match(CHR_TOK)) return LEAVE_FAIL;
+        break;
+      default:
+        //ParseError("No option for type");
+        return LEAVE_FAIL;
+    }
+    return LEAVE_SUCC;
 }
 
 bool IDLst()
 // <IDLst>   :== Ident <MLst>
 {
+    ENTER;
+    if(!Match(IDENT_TOK)) return LEAVE_FAIL;
+    if(!MLst()) return LEAVE_FAIL;
+    return LEAVE_SUCC;
 }
 
 bool MLst()
 // <MLst>    :== , <IDLst>
 // <MLst>    :==
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case COMMA_TOK:
+          if(!Match(COMMA_TOK)) return LEAVE_FAIL;
+          if(!IDLst()) return LEAVE_FAIL;
+          break;
+        case SEMI_TOK:
+          break;
+        default:
+          //ParseError("No opt in MLst");
+          return LEAVE_FAIL;
+          //break;
+    }
+    return LEAVE_SUCC;
 }
 
 bool Assign()
 // <Assign>  :==  <Ident> := <Expr>
 {
+    ENTER;
+    if(!Match(IDENT_TOK)) return LEAVE_FAIL;
+    if(!Match(ASSIGN_TOK)) return LEAVE_FAIL;
+    if(!Expr()) return LEAVE_FAIL;
+    return LEAVE_SUCC;
 }
 
 bool Expr()
 // <Expr>    :==  <Term> <MExpr>
 {
+    ENTER;
+    if(!Term()) return LEAVE_FAIL;
+    if(!MExpr()) return LEAVE_FAIL;
+    return LEAVE_SUCC;
 }
 
 bool MExpr()
 // <MExpr>   :==  <AddOp> <Term> <MExpr>
 // <MExpr>   :==
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case PLUS_TOK:
+        case MINUS_TOK:
+          if(!AddOp()) return LEAVE_FAIL;
+          if(!Term()) return LEAVE_FAIL;
+          if(!MExpr()) return LEAVE_FAIL;
+          break;
+        case SEMI_TOK:
+        case RPAREN_TOK:
+          break;
+        default:
+          ParseError("Parse fail in MExpr");
+          return LEAVE_FAIL;
+          break;
+    }
+    return LEAVE_SUCC;
 }
 
 bool Term()
 //<Term>     :==  <Factor> <MTerm>
 {
+    ENTER;
+    if(!Factor()) return LEAVE_FAIL;
+    if(!MTerm()) return LEAVE_FAIL;
+    return MTerm();
 }
 
 bool MTerm()
 //<MTerm>    :==  <MultOp> <Factor> <MTerm>
 //<MTerm>    :==
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case TIMES_TOK:
+        case DIV_TOK:
+          if(!MultOp()) return LEAVE_FAIL;
+          if(!Factor()) return LEAVE_FAIL;
+          if(!MTerm()) return LEAVE_FAIL;
+          break;
+        case MINUS_TOK:
+        case PLUS_TOK:
+        case SEMI_TOK:
+        case RPAREN_TOK:
+          //if(!MultOp()) return LEAVE_FAIL;
+          break;
+        default:
+          ParseError("Fail on MTerm");
+          return LEAVE_FAIL;
+          break;
+    }
+    return LEAVE_SUCC;
 }
 
 bool Factor()
@@ -104,16 +190,71 @@ bool Factor()
 // <Factor>  :==  <FloatLit>
 // <Factor>  :==  <Ident>
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case LPAREN_TOK:
+          if(!Match(LPAREN_TOK)) return LEAVE_FAIL;
+          if(!Expr()) return LEAVE_FAIL;
+          if(!Match(RPAREN_TOK)) return LEAVE_FAIL;
+          break;
+        case MINUS_TOK:
+          if(!Match(MINUS_TOK)) return LEAVE_FAIL;
+          if(!Factor()) return LEAVE_FAIL;
+          break;
+        case INTLIT_TOK:
+          if(!Match(INTLIT_TOK)) return LEAVE_FAIL;
+          break;
+        case FLOATLIT_TOK:
+          if(!Match(FLOATLIT_TOK)) return LEAVE_FAIL;
+          break;
+        case IDENT_TOK:
+          if(!Match(IDENT_TOK)) return LEAVE_FAIL;
+          break;
+        default:
+          ParseError("No option in switch");
+          return LEAVE_FAIL;
+    }
+    return LEAVE_SUCC;
 }
 
 bool AddOp()
 // <AddOp>    :==  -
 // <AddOp>    :==  +
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case MINUS_TOK:
+          Match(MINUS_TOK);
+          break;
+        case PLUS_TOK:
+          Match(PLUS_TOK);
+          break;
+        default:
+          ParseError("AddOp not recognized");
+          return LEAVE_FAIL;
+    }
+    //if(Match(MINUS_TOK)) return LEAVE_SUCC;
+    //if(Match(PLUS_TOK)) return LEAVE_SUCC;
+    return LEAVE_SUCC;
 }
 
 bool MultOp()
 // <MultOp>   :==  *
 // <MultOp>   :==  /
 {
+    ENTER;
+    switch(CurrentToken()) {
+        case TIMES_TOK:
+          Match(TIMES_TOK);
+          break;
+        case DIV_TOK:
+          Match(DIV_TOK);
+          break;
+        default:
+          ParseError("MultOp not recognized");
+          return LEAVE_FAIL;
+    }
+    //if(Match(TIMES_TOK)) return LEAVE_SUCC;
+    //if(Match(DIV_TOK)) return LEAVE_SUCC;
+    return LEAVE_SUCC;
 }
